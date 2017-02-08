2e5f3aff4d82ea19d01cdb21aff18c0aa104b7bb
Harry Helmrich <harryhelmrich@harrys-mbp.fios-router.home>
2017-02-07 21:23:19 -0500

    Further configuration of work order view controllers

--- CMMS App/WorkOrderListTableViewController.swift
+++ CMMS App/WorkOrderListTableViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class WorkOrderListTableViewController: UITableViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,25 +27,25 @@ class WorkOrderListTableViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+/*
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 3
+        return 1
     }
-
+*/
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 5
+        return workorderNumbers.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
-        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        cell.textLabel?.text = workorderDescriptions[indexPath.row]
         return cell
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Section \(section)"
+        return "Harry's House"
     }
 
 }
