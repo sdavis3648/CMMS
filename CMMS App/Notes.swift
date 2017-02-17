8ef05bf609dd1f0817966f3f59944f6b4a7f1936
Harry Helmrich <harryhelmrich@harrys-mbp.fios-router.home>
2017-02-16 22:12:36 -0500

    Added notes file

---
+++ CMMS App/Notes.swift
@@ -0,0 +1,42 @@
+//
+//  Notes.swift
+//  CMMS App
+//
+//  Created by Harry Helmrich on 2/16/17.
+//  Copyright © 2017 Spencer Davis. All rights reserved.
+//
+
+import Foundation
+/*
+ To-Do items:
+ 
+ ***Create Work Order View Controller****
+ Add photo to save to Firebase
+ Make ImageView look good, aspects/constraints
+ Priority button needs to give out Integer, not string
+ Work Order name auto-fill
+ Name would auto-fill
+ Set up users within authentication in Firebase
+ Re-define "Date" to "Completion Date" and add current date/timestamp when saved to Firebase
+ Create database of Assets, have Location auto-populate from that database
+ After closing popUp, return to Dashboard
+ Loading bar linked to Firebase to confirm loaded to database
+ Status indentifier (open and closed)
+ 
+ ***View Work Order****
+ Pull data from Firebase into Tableview cells
+ Add edit/save button
+ Customize TableViewCell for expandable info and mini-image
+ Ability to close work order
+ Add 4th priority
+ Table view list is filterable or searchable
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
